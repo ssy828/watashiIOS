94a57d8ad48a30aa3f12e5d8969c4b921d1b1eef
ssy828 <828yss@gmail.com>
2017-11-07 12:57:46 +0900

    Modified AutoLayout to Ratio

--- 練習/NewMP/MyMusicPlayer/NewLogin/Layout/RoundButton.swift
+++ 練習/NewMP/MyMusicPlayer/NewLogin/Layout/RoundButton.swift
@@ -4,15 +4,17 @@
 //
 
 import UIKit
-@IBDesignable
+
 class RoundButton: UIButton {
     
     // MARK: 스토리보드에서 사용할때 쓰는 init
     // 스토리보드 코드 : NSCoder
     
-    required convenience init?(coder aDecoder: NSCoder) {
-        self.init(coder: aDecoder)
-        self.layer.cornerRadius = 20
+    required init?(coder aDecoder: NSCoder)
+    {
+        // UIButton을 상속받으므로 super!
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 10
       
     }
     
@@ -20,11 +22,11 @@ class RoundButton: UIButton {
     // @IBInspectable: inspector에서 해당 인터페이스 요소의 속성을 변경가능하게 하는 것!
     // @IBDesignable: 실시간을 시각적으로 보기 위한 용도
     // 근데 많이 무거워짐
-    var corner: CGFloat = 0 {
-        didSet {
-            self.layer.cornerRadius = corner
-        }
-    }
+//   @IBInspectable var corner: CGFloat = 0 {
+//        didSet {
+//            self.layer.cornerRadius = corner
+//        }
+//    }
     // 연산프로퍼티: get - 그때 값을 set에서 새로 만들때 사용
     
     
