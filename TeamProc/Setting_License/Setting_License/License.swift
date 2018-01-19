1de676be4898c2376452179c28f3e62c744ae89d
ssy828 <828yss@gmail.com>
2018-01-19 17:19:14 +0900

    modifying code

--- TeamProc/Setting_License/Setting_License/License.swift
+++ TeamProc/Setting_License/Setting_License/License.swift
@@ -7,29 +7,25 @@ import Foundation
 import UIKit
 
 class License {
-    //    var githubAddress: String = ""
-    var sentence = ""
-    let introductionOfLicense = "The MIT License (MIT)"
-    let conclusionOfLicense = """
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    //    var items: [LicenseItem]
+    var text = ""
     
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    """
-    
-    public func configureLicenseSentence(_ ofGitubAddress: String?,
-                                      _ textView: UITextView) -> String {
-        guard let gitubAddress = ofGitubAddress else { return ""}
-        sentence =  introductionOfLicense + "\n" +
-                    "(\(gitubAddress))" + "\n" +
-                    "\n" + conclusionOfLicense
-        attributedOfLink(mutableString: sentence, ofValue: gitubAddress, range: gitubAddress, textView: textView)
-        return sentence
+    // MARK: 라이선스를 문장으로 만드는 메소드
+    private func configureText(to item: LicenseItem, _ ofGitubAddress: String?) -> String {
+        if let gitubAddress = ofGitubAddress {
+            text =  item.introductionOfLicense + "\n" +
+                "Copyright (c)" + "\(item.licenseYear)" + "\(item.name)" +
+                "\n" + "(\(gitubAddress))" + "\n" +
+                "\n" + item.licenseText
+        } else {
+            text =  item.introductionOfLicense + "\n" +
+                "Copyright (c)" + "\(item.licenseYear)" + "\(item.name)" +
+                "\n" + item.licenseText
+        }
+        return text
     }
     
+    //MARK: 특정 문자열을 링크 연결 메소드
     private func attributedOfLink(mutableString string: String,
                                   ofValue: String,
                                   range ofSearchString: String,
@@ -39,4 +35,6 @@ class License {
         ofLicense.attributedText = attributedString
     }
     
+    
 }
+
--- TeamProc/Setting_License/Setting_License/VC/LicenseViewController.swift
+++ TeamProc/Setting_License/Setting_License/VC/LicenseViewController.swift
@@ -7,14 +7,12 @@ import UIKit
 class LicenseViewController: UIViewController {
     
     @IBOutlet weak var licenseTextView: UITextView!
-    let licneseInstance = License()
     var test: String?
     
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         //        let _ = licneseInstance.makeSentenceOfLicense("https://github.com/onevcat/Kingfisher", licenseTextView)
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
