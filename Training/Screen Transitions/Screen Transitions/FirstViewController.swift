f0db44c2cd098c502e4c5deb3b77e30b041f2153
ssy828 <828yss@gmail.com>
2018-05-11 20:54:53 +0900

    trained Unwind Segue

---
+++ Training/Screen Transitions/Screen Transitions/FirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  Screen Transitions
+//
+//  Created by SSY on 2018. 5. 11..
+//  Copyright © 2018년 LittleMe. All rights reserved.
+//
+
+import UIKit
+class FirstViewController: UIViewController {
+
+    // Unwind Segue
+    @IBAction func goToHomePage(_ sender: UIStoryboardSegue){ }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
