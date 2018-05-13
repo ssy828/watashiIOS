2bb3981c1d6cabe244215db00e4de84d78c5c37f
ssy828 <828yss@gmail.com>
2018-05-13 23:11:59 +0900

    note about local notification

--- Training/Screen Transitions/Screen Transitions/AppDelegate.swift
+++ Training/Screen Transitions/Screen Transitions/AppDelegate.swift
@@ -7,40 +7,69 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if #available(iOS 10.0, *){ // UserNotification 프레임 워크를 이용한 로컬 알림(ios 10 이상) -> ios 10 이상 버전은 if 조건절 실행
+            // 알림 동의 여부 확인
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                if settings.authorizationStatus == UNAuthorizationStatus.authorized{
+                    // 알림 콘텐츠 인스턴스
+                    let nContent = UNMutableNotificationContent()
+                    nContent.badge = 1
+                    nContent.title = "로컬 알림 메시지"
+                    nContent.subtitle = "다시 열어주세요~"
+                    nContent.body = "왜 나갔어요! 다시 들와 주세요~"
+                    nContent.sound = UNNotificationSound.default()
+                    nContent.userInfo = ["name":"가나다"]
+                    
+                    // 알림 발송 조건 인스턴스
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    
+                    // 알림 요청 인스턴스
+                    // 식별 아이디: 여러 개의 알림 요청 중에서 원하는 것을 식별하는 용도
+                    let request = UNNotificationRequest(identifier: "일어나", content: nContent, trigger: trigger)
+                    
+                    // 노티피케이션 센터에 추가
+                    UNUserNotificationCenter.current().add(request)
+                } else{
+                    print("사용자가 동의 하지 않음")
+                }
+            }
+            
+        }else{ // ios 9이하 UILocalNotification 이용
+            
+        }
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- Training/Screen Transitions/Screen Transitions/UnwindSegue/FirstViewController.swift
+++ Training/Screen Transitions/Screen Transitions/UnwindSegue/FirstViewController.swift
@@ -8,24 +8,24 @@
 
 import UIKit
 class FirstViewController: UIViewController {
-
+    
     // Unwind Segue
     @IBAction func goToHomePage(_ sender: UIStoryboardSegue){ }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
